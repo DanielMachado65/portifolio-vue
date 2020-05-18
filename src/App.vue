701fbf049add58b07480fbee0ec274a98d76acc6
daniel machado <danielmachadopintos@gmail.com>
2020-05-17 22:38:02 -0300

    feat(webpack): add sass

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <Header />
-    <Menu />
     <Body />
   </div>
 </template>
 
 <script>
-import { Header, Menu, Body } from "./components/index.js";
+import { Header, Body } from "./components/index.js";
 
 export default {
   name: "App",
-  components: { Header, Menu, Body }
+  components: { Header, Body }
 };
 </script>
 
