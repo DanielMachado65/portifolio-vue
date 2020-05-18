7aa1e037e108b23cf2877facdff184ffca167312
daniel machado <danielmachadopintos@gmail.com>
2020-05-17 23:29:13 -0300

    feat(parallax): add parallax slot

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -1,42 +1,44 @@
 <template>
-  <div class="parallax">
-    <div class="parallax__layer parallax__layer__0">
+  <header>
+    <div class="parallax-layer layer-0">
       <img
         src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_0.png?raw=true"
       />
     </div>
-    <div class="parallax__layer parallax__layer__1">
+    <div class="parallax-layer layer-1">
       <img
         src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_1.png?raw=true"
       />
     </div>
-    <div class="parallax__layer parallax__layer__2">
+    <div class="parallax-layer layer-2">
       <img
         src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_2.png?raw=true"
       />
     </div>
-    <div class="parallax__layer parallax__layer__3">
+    <div class="parallax-layer layer-3">
       <img
         src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_3.png?raw=true"
       />
     </div>
-    <div class="parallax__layer parallax__layer__4">
+    <div class="parallax-layer layer-4">
       <img
         src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_4.png?raw=true"
       />
     </div>
-    <div class="parallax__layer parallax__layer__5">
+    <div class="parallax-layer layer-5">
       <img
         src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_5.png?raw=true"
       />
     </div>
-    <div class="parallax__layer parallax__layer__6">
+    <div class="parallax-layer layer-6">
       <img
         src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_6.png?raw=true"
       />
     </div>
-    <div class="parallax__cover"></div>
-  </div>
+    <main>
+      <slot></slot>
+    </main>
+  </header>
 </template>
 
 <script>
@@ -46,7 +48,7 @@ export default {
 </script>
 
 <style scoped>
-.parallax {
+header {
   perspective: 100px;
   height: 100vh;
   overflow-x: hidden;
@@ -59,21 +61,7 @@ export default {
   margin-left: -1500px;
 }
 
-.parallax__layer {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-}
-
-.parallax__layer img {
-  display: block;
-  position: absolute;
-  bottom: 0;
-}
-
-.parallax__cover {
+main {
   background: #2d112b;
   display: block;
   position: absolute;
@@ -84,25 +72,39 @@ export default {
   z-index: 2;
 }
 
-.parallax__layer__0 {
+.parallax-layer {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.parallax-layer img {
+  display: block;
+  position: absolute;
+  bottom: 0;
+}
+
+.layer-0 {
   transform: translateZ(-300px) scale(4);
 }
-.parallax__layer__1 {
+.layer-1 {
   transform: translateZ(-250px) scale(3.5);
 }
-.parallax__layer__2 {
+.layer-2 {
   transform: translateZ(-200px) scale(3);
 }
-.parallax__layer__3 {
+.layer-3 {
   transform: translateZ(-150px) scale(2.5);
 }
-.parallax__layer__4 {
+.layer-4 {
   transform: translateZ(-100px) scale(2);
 }
-.parallax__layer__5 {
+.layer-5 {
   transform: translateZ(-50px) scale(1.5);
 }
-.parallax__layer__6 {
+.layer-6 {
   transform: translateZ(0px) scale(1);
 }
 </style>
