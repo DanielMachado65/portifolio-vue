b868aa4061808dd0a68e6e733e1789191ed2ff68
daniel machado <danielmachadopintos@gmail.com>
2020-05-29 00:09:47 -0300

    feat(infoContainer): add info container music

--- src/components/InfoContainer.vue
+++ src/components/InfoContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="parallax-group info-container">
-    <img src="../assets/main.png" alt="Lively and colourful concert" />
     <div class="text-container">
+      <iframe src="https://open.spotify.com/embed/track/0PXukVbYpvz40KcEFKnIw7" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       <h2>This is your year.</h2>
       <p>What would life be if we had no courage to attempt anything?</p>
       <p>- Vincent Van Gogh</p>
@@ -19,9 +19,8 @@ export default {
 <style>
 .info-container {
   background: #000;
-  transform: translateZ(0) scale(1);
-  z-index: 8;
   display: flex;
+  
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -48,20 +47,5 @@ export default {
 .text-container p:nth-of-type(2) {
   font-size: 0.6rem;
   margin-bottom: 3rem;
-}
-
-@media (min-width: 760px) {
-  .info-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 3rem;
-    align-items: center;
-    text-align: left;
-  }
-
-  .info-container img {
-    justify-self: end;
-    width: 75%;
-  }
 }
 </style>
